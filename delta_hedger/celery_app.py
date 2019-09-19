9022129d7d274318df65b9c4146a69f36d49e580
pavelkrolevets <pavelkrolevets@gmail.com>
2019-09-19 13:13:15 +0800

    fix: celery tasks piks up well

--- delta_hedger/celery_app.py
+++ delta_hedger/celery_app.py
@@ -1,12 +1,9 @@
 from __future__ import absolute_import
 from celery import Celery
-from celery.utils.log import get_task_logger
 
-
-celery_app = Celery('delta_hedger')
+celery_app = Celery('delta_hedger', include=['delta_hedger.tasks'])
 celery_app.conf.broker_url = 'redis://localhost:6379/0'
 celery_app.conf.result_backend = 'redis://localhost:6379/0'
 
-logger = get_task_logger(__name__)
 if __name__ == '__main__':
     celery_app.start()
